ede87b25bd93d34979e91c546ac12a0dcb70697b
ghkdtnql <ghkdtnql@gmail.com>
2025-01-24 15:22:46 +0900

    test: ci/cd test

--- backend/overthecam/src/main/java/com/overthecam/OverthecamApplication.java
+++ backend/overthecam/src/main/java/com/overthecam/OverthecamApplication.java
@@ -1,4 +1,4 @@
-package com.yes.overthecam;
+package com.overthecam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OverthecamApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(OverthecamApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(OverthecamApplication.class, args);
+	}
 
 }
--- backend/overthecam/src/test/java/com/overthecam/OverthecamApplicationTests.java
+++ backend/overthecam/src/test/java/com/overthecam/OverthecamApplicationTests.java
@@ -1,4 +1,4 @@
-package com.yes.overthecam;
+package com.overthecam;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class OverthecamApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }

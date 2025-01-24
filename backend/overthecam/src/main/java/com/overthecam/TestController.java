5a3e29127ece4f3057b0db846b5f48d2afccf11b
ghkdtnql <ghkdtnql@gmail.com>
2025-01-25 04:44:54 +0900

    test: / ã에 대한 주소 test controller method add

--- backend/overthecam/src/main/java/com/overthecam/TestController.java
+++ backend/overthecam/src/main/java/com/overthecam/TestController.java
@@ -1,10 +1,16 @@
 package com.overthecam;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+
+    @GetMapping("/")
+    public String root() {
+        return "root ok";
+    }
 
     @RequestMapping("/test")
     public String test(){

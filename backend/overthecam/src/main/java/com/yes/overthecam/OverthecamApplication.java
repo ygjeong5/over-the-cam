0c18b7d21bd9791e85a46640df88323e83856137
ghkdtnql <ghkdtnql@gmail.com>
2025-01-24 12:57:36 +0900

    chore: backend project folder 및 ãproject 생성

---
+++ backend/overthecam/src/main/java/com/yes/overthecam/OverthecamApplication.java
@@ -0,0 +1,13 @@
+package com.yes.overthecam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverthecamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OverthecamApplication.class, args);
+    }
+
+}
---
+++ backend/overthecam/src/test/java/com/yes/overthecam/OverthecamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yes.overthecam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverthecamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

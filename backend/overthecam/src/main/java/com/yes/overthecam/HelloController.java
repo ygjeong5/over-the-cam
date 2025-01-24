20871181eacff8cbd643f6ec7650720ac00fb6fa
ghkdtnql <ghkdtnql@gmail.com>
2025-01-24 14:57:58 +0900

    test: ci/cd test

---
+++ backend/overthecam/src/main/java/com/yes/overthecam/HelloController.java
@@ -0,0 +1,13 @@
+package com.yes.overthecam;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, Over The Cam!";
+    }
+}

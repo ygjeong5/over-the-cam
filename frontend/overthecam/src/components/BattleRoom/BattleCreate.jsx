81e9d3ad6b60b640d0f46bfb8b07741a43da0d3a
Sunhwa Sim <sh227sim@gmail.com>
2025-01-27 00:33:11 +0900

    feat: 배틀 생성 방 페이지 라우터 생성

---
+++ frontend/overthecam/src/components/BattleRoom/BattleCreate.jsx
@@ -0,0 +1,9 @@
+function BattleCreate() {
+    return (
+        <div>
+            <h1>Create Battle Room</h1>
+        </div>
+    )
+}
+
+export default BattleCreate;
--- frontend/overthecam/src/main.jsx
+++ frontend/overthecam/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
+import BattleCreate from './components/BattleRoom/BattleCreate.jsx'
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path:'create-battle-room', element: <BattleCreate /> },
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
   </StrictMode>,
 )

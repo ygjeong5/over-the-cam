751129a739f437c5a58bd86dcb1c5df15e7d5875
asj7090 <asj7090@gmail.com>
2025-01-28 00:05:51 +0900

    :recycle: 예외 처리와 공통 응답 패키지 구조 분리

--- backend/overthecam/src/main/java/com/overthecam/exception/GlobalExceptionHandler.java
+++ backend/overthecam/src/main/java/com/overthecam/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
-package com.overthecam.common.exception;
+package com.overthecam.exception;
 
+import com.overthecam.common.dto.CommonResponseDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -11,15 +12,15 @@ public class GlobalExceptionHandler {
 
     // 예외 처리하는 메소드
     @ExceptionHandler(value = GlobalException.class)
-    public ResponseEntity<GlobalResponseDto<Object>> handleGlobalException(GlobalException e) {
+    public ResponseEntity<CommonResponseDto<Object>> handleGlobalException(GlobalException e) {
         // 로그에 상세 정보 남기기
         log.error("Error occurred: {} - {}", e.getMessage(), e.getDetail());
 
         return createErrorResponse(e);
     }
 
-    private ResponseEntity<GlobalResponseDto<Object>> createErrorResponse(GlobalException e) {
-        GlobalResponseDto<Object> response = GlobalResponseDto.builder()
+    private ResponseEntity<CommonResponseDto<Object>> createErrorResponse(GlobalException e) {
+        CommonResponseDto<Object> response = CommonResponseDto.builder()
                 .code(String.valueOf(e.getHttpStatus().value()))
                 .message(e.getMessage())
                 .build();

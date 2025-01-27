031a5046dca5a184332ec56305ffbfcf7b620918
asj7090 <asj7090@gmail.com>
2025-01-28 02:38:42 +0900

    :sparkles: add: 예외 처리 추가

--- backend/overthecam/src/main/java/com/overthecam/exception/GlobalExceptionHandler.java
+++ backend/overthecam/src/main/java/com/overthecam/exception/GlobalExceptionHandler.java
@@ -2,34 +2,43 @@ package com.overthecam.exception;
 
 import com.overthecam.common.dto.CommonResponseDto;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
 
 @Slf4j
-@RestControllerAdvice("/api")
+@RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    // 예외 처리하는 메소드
     @ExceptionHandler(value = GlobalException.class)
     public ResponseEntity<CommonResponseDto<Object>> handleGlobalException(GlobalException e) {
-        // 로그에 상세 정보 남기기
-        log.error("Error occurred: {} - {}", e.getMessage(), e.getDetail());
-
-        return createErrorResponse(e);
-    }
-
-    private ResponseEntity<CommonResponseDto<Object>> createErrorResponse(GlobalException e) {
-        CommonResponseDto<Object> response = CommonResponseDto.builder()
-                .code(String.valueOf(e.getHttpStatus().value()))
-                .message(e.getMessage())
-                .build();
+        log.error("Global Exception occurred: {} - {}", e.getErrorCode(), e.getDetail());
 
         return ResponseEntity
-                .status(e.getHttpStatus())
-                .body(response);
+            .status(e.getErrorCode().getHttpStatus())
+            .body(CommonResponseDto.error(e.getErrorCode()));
     }
 
+    // 기본 예외 처리 추가
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<CommonResponseDto<Object>> handleException(Exception e) {
+        log.error("Unexpected Exception occurred:", e);
 
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(CommonResponseDto.error(ErrorCode.INTERNAL_SERVER_ERROR));
+    }
+
+    // Resource Not Found 예외 처리 추가
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<CommonResponseDto<Object>> handleNoResourceFoundException(
+        NoResourceFoundException e) {
+        log.error("Resource not found:", e);
+
+        return ResponseEntity
+            .status(HttpStatus.NOT_FOUND)
+            .body(CommonResponseDto.error(ErrorCode.INVALID_INPUT_VALUE));
+    }
 }
-

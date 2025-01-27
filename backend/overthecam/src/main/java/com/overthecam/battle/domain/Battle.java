86635ee564feeeaf9efe55cd0dfcd60f3ec0ece9
asj7090 <asj7090@gmail.com>
2025-01-27 22:35:12 +0900

    :sparkles: feat: 채팅방 생성 기능

---
+++ backend/overthecam/src/main/java/com/overthecam/battle/domain/Battle.java
@@ -0,0 +1,46 @@
+package com.overthecam.battle.domain;
+
+import com.overthecam.common.entity.TimeStampEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Builder
+@Table(name = "battle")
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Battle extends TimeStampEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "battle_id")
+    private Long id;
+
+    //@ManyToOne(fetch = FetchType.LAZY)
+    //@JoinColumn(name = "user_id", nullable = false)
+    //private User user;  // 방장 사용자
+    private Long userId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "room_url", nullable = false)
+    private String roomUrl;
+
+    @Column(name = "thumbnail_url", nullable = false)
+    private String thumbnailUrl;
+
+    @Column(name = "total_time")
+    private Integer totalTime;
+
+    @Column(name = "user_count")
+    private Integer userCount = 0;
+
+    @Column(name = "status")
+    private Integer status = 0;
+
+}
+

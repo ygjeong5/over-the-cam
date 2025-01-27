81b1caf8fafb245b03be2bf2cb206b94de0d61b9
ghkdtnql <ghkdtnql@gmail.com>
2025-01-27 19:09:48 +0900

    feat: global error v1 implememtation

--- backend/overthecam/src/main/java/com/overthecam/common/exception/GlobalException.java
+++ backend/overthecam/src/main/java/com/overthecam/common/exception/GlobalException.java
@@ -1,29 +1,19 @@
 package com.overthecam.common.exception;
 
-import com.overthecam.common.Constants;
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter
 public class GlobalException extends Exception {
 
-    private Constants.ExceptionClass exceptionClass;
-    private HttpStatus httpStatus;
+    private final HttpStatus httpStatus;
 
-    public GlobalException(Constants.ExceptionClass exceptionClass, HttpStatus httpStatus, String message) {
-        super(exceptionClass.toString() + message); //constants의 toString 결과값에 detail message를 더한다.
-        this.exceptionClass = exceptionClass;
+    public GlobalException(HttpStatus httpStatus) {
         this.httpStatus = httpStatus;
     }
 
     public int getHttpStatusCode() {
         return httpStatus.value();
     } //400과 같은 에러 코드를 리턴시킨다.
-
-    public String getHttpStatusType() {
-        return httpStatus.getReasonPhrase();
-    }
-
-    public HttpStatus getHttpStatus() {
-        return httpStatus; //객체 자체를 리턴시킨다.
-    }
 
 }

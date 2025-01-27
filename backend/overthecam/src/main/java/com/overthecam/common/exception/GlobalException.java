a15df6e3015e76b947b4608e13793ab0310c4b90
ghkdtnql <ghkdtnql@gmail.com>
2025-01-27 22:18:44 +0900

    fix: createErrorResponse ãmethod 분리

--- backend/overthecam/src/main/java/com/overthecam/common/exception/GlobalException.java
+++ backend/overthecam/src/main/java/com/overthecam/common/exception/GlobalException.java
@@ -6,7 +6,6 @@ import org.springframework.http.HttpStatus;
 @Getter
 public class GlobalException extends RuntimeException {
     private final ErrorCode errorCode;
-    @Getter
     private final String detail; //Client에는 전달되지 않고, 개발자들이 세부사항을 보기 위한 문자열.
 
     public GlobalException(ErrorCode errorCode, String detail) {
--- backend/overthecam/src/main/java/com/overthecam/common/exception/GlobalExceptionHandler.java
+++ backend/overthecam/src/main/java/com/overthecam/common/exception/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
 package com.overthecam.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,34 +9,26 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice("/api")
 public class GlobalExceptionHandler {
 
-    // 성공 응답을 처리하는 메소드
-    protected <T> ResponseEntity<GlobalResponseDto<T>> success(T data, String message) {
-        GlobalResponseDto<T> response = GlobalResponseDto.<T>builder()
-                .code(String.valueOf(HttpStatus.OK.value()))  // "200"
-                .message(message)
-                .data(data)
-                .build();
-
-        return ResponseEntity.ok(response);
-    }
-
     // 예외 처리하는 메소드
     @ExceptionHandler(value = GlobalException.class)
     public ResponseEntity<GlobalResponseDto<Object>> handleGlobalException(GlobalException e) {
-
         // 로그에 상세 정보 남기기
         log.error("Error occurred: {} - {}", e.getMessage(), e.getDetail());
 
+        return createErrorResponse(e);
+    }
 
+    private ResponseEntity<GlobalResponseDto<Object>> createErrorResponse(GlobalException e) {
         GlobalResponseDto<Object> response = GlobalResponseDto.builder()
-                .code(String.valueOf(e.getHttpStatus().value()))  // HTTP 상태 코드를 문자열로 변환 (e.g., "404")
-                .message(e.getMessage()) //client에게는 미리 정의된 메시지만 보여주자.
-                //.detail(e.getDetail())
+                .code(String.valueOf(e.getHttpStatus().value()))
+                .message(e.getMessage())
                 .build();
 
         return ResponseEntity
                 .status(e.getHttpStatus())
                 .body(response);
     }
+
+
 }
 

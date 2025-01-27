c50773402ae7030865d1a91e27b3c800d44bfcc8
Sunhwa Sim <sh227sim@gmail.com>
2025-01-27 11:49:42 +0900

    refactor: 프리티어 적용

--- frontend/overthecam/src/components/BattleRoom/BattleCreating.jsx
+++ frontend/overthecam/src/components/BattleRoom/BattleCreating.jsx
@@ -1,30 +1,36 @@
+import { useNavigate } from "react-router-dom";
+
 import BattleCreateForm from "./BattleCreatingForm";
 
 function BattleCreate() {
+  const navigate = useNavigate();
 
-    function createBattle() {
-        // post 방 생성
-        // post 방장 부여
-        // post 방 id
-        // post 방 제목 
-    }
+  function createBattle() {
+    // post 방 생성
+    // post 방장 부여
+    // post 방 id
+    // post 방 제목
+  }
 
-    function createBattleRoomHandler(event) {
-        event.preventDefault();
-        createBattle();
-        console.log("방을 생성 합니다");
-        // createBattle() 호출
-        // battle-room/:battleId 로 이동
-    }
+  function createBattleRoomHandler(event) {
+    event.preventDefault();
+    createBattle();
+    console.log("방을 생성 합니다");
+    // createBattle() 호출
 
+    // 방 생성 후 방 아이디 받아오기
+    const battleId = "1234";
+    // battle-room/:battleId 로 이동
+    navigate(`/battle-room/${battleId}`);
+  }
 
-    return (
-        <div>
-            <h1>방 만들기</h1>
-            <p>지금 바로 배틀 방을 만들고 논쟁을 즐겨보세요!</p>
-            <BattleCreateForm onCreateRoom={createBattleRoomHandler} />
-        </div>
-    )
+  return (
+    <div>
+      <h1>방 만들기</h1>
+      <p>지금 바로 배틀 방을 만들고 논쟁을 즐겨보세요!</p>
+      <BattleCreateForm onCreateRoom={createBattleRoomHandler} />
+    </div>
+  );
 }
 
 export default BattleCreate;

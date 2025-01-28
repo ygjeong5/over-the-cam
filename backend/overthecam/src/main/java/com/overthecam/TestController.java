ecdbb51f0e5f4b6853498345d84652716b6820ce
asj7090 <asj7090@gmail.com>
2025-01-28 10:33:34 +0900

    :white_check_mark: test: 예외 처리 테스트 컨트롤러

--- backend/overthecam/src/main/java/com/overthecam/TestController.java
+++ backend/overthecam/src/main/java/com/overthecam/TestController.java
@@ -1,6 +1,11 @@
 package com.overthecam;
 
+import com.overthecam.common.dto.CommonResponseDto;
+import com.overthecam.exception.ErrorCode;
+import com.overthecam.exception.GlobalException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +17,16 @@ public class TestController {
         return "root ok";
     }
 
-    @RequestMapping("/test")
+    @RequestMapping("/test/cicd")
     public String test(){
         return "ci/cd test ok~~";
+    }
+
+    @GetMapping("/test/error")
+    public ResponseEntity<CommonResponseDto<Object>> testUserNotFound() {
+        throw new GlobalException(
+            ErrorCode.USER_NOT_FOUND,
+            "테스트용 사용자를 찾을 수 없습니다"
+        );
     }
 }

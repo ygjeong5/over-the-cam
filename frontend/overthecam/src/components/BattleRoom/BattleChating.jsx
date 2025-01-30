151e763e098d38ab50166e86b57a7bb3425a6542
Sunhwa Sim <sh227sim@gmail.com>
2025-01-30 15:08:11 +0900

    feat: 채팅창 컴포넌트 제작

--- frontend/overthecam/src/components/BattleRoom/BattleChating.jsx
+++ frontend/overthecam/src/components/BattleRoom/BattleChating.jsx
@@ -1,5 +1,13 @@
 function BattleChating() {
-    
+  return <div>
+    <p>실시간 채팅</p>
+    <div>
+        <div>
+            <input type="text" placeholder="내용을 입력하세요..."/>
+            <input type="button" value="보내기기" />
+        </div>
+    </div>
+  </div>;
 }
 
 export default BattleChating;

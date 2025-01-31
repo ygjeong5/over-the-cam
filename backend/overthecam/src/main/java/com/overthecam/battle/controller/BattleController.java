77f2c73794d88e83b3f7d0c2d3b378169e024123
ghkdtnql <ghkdtnql@gmail.com>
2025-02-01 03:15:19 +0900

    🔧feat: 초ê: 초기에 방을 생성하기 위한 controller 구현

--- backend/overthecam/src/main/java/com/overthecam/battle/controller/BattleController.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/controller/BattleController.java
@@ -1,16 +1,28 @@
 package com.overthecam.battle.controller;
 
+import com.overthecam.common.dto.CommonResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 초기에 방을 생성할때 -> 사용자가 우리 회원일 경우 -> secret-key로 openvidu에게 방 생성요청
+ * openvidu에게 session_id 받은 후 우리 db에 저장 (battle_id에 대한 session_id가 1:1)
+ * 사용자 token을 만들어서 front에게 token, session_id..? 전송
+ * 방장이든 아니든 발급된 token과 sessionId로 방 접속 허락
+ */
 
 @RestController
 @RequestMapping("/api/battles")
 @RequiredArgsConstructor
 public class BattleController {
 
-    private final BattleService battleService;
-    private final String OPENVIDU_URL = "${openvidu.url}";
-    private final String SECRET = "${openvidu.secret}";
+    @PostMapping // 맨 처음 한 번 방장이 방을 만들 때 secret-key 로 openvidu에게 방 생성 요청을 보내는 컨트롤러
+    public ResponseEntity<?> createBattle(@AuthenticationPrincipal UserDetails userDetails) {
+        String sessionId = battleService.createBattleSession(Long.parseLong(userDetails.getUsername()));
+        return ResponseEntity.ok(CommonResponseDto.success("sessionId", sessionId));
+    }
 
 }

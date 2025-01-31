c2d73fe2627c2005f8d655297a41061ccbb59a49
shsim <sh227sim@gmail.com>
2025-02-01 00:11:22 +0900

    🚨 fix: vite 와 sockJs 사이 global 정의 문제 해결

--- frontend/overthecam/vite.config.js
+++ frontend/overthecam/vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    global: 'globalThis', // 🔥 글로벌 객체를 globalThis로 대체
+  },
 })

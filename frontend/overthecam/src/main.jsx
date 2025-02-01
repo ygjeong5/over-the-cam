22bc449566e846f0bd248d3b28e9f60f72406331
shsim <sh227sim@gmail.com>
2025-02-01 19:51:18 +0900

    ✨ feat: 상점 페이지 라우터 설정

--- frontend/overthecam/src/main.jsx
+++ frontend/overthecam/src/main.jsx
@@ -6,14 +6,16 @@ import App from "./App.jsx";
 import BattleCreate from "./page/BattleRoom/BattleCreatingPage.jsx";
 import BattleWaiting from "./page/BattleRoom/BattleRoomPage.jsx";
 import BattleMainPage from "./page/BattleRoom/BattleMainPage.jsx";
+import ItemShopPage from "./page/ItemShop/ItemShopPage.jsx";
 
 const router = createBrowserRouter([
   { path: "/", element: <App /> },
-  { path: "/battle-list", element: <BattleMainPage/>},
+  { path: "/battle-list", element: <BattleMainPage /> },
   { path: "/create-battle-room", element: <BattleCreate /> },
   { path: "/battle-room/:battleId", element: <BattleWaiting /> },
+  { path: "/store", element: <ItemShopPage /> },
 ]);
 
 createRoot(document.getElementById("root")).render(
-    <RouterProvider router={router} />
+  <RouterProvider router={router} />
 );
--- frontend/overthecam/src/page/ItemShop/ItemShopPage.jsx
+++ frontend/overthecam/src/page/ItemShop/ItemShopPage.jsx
@@ -1,5 +1,10 @@
-function ItemShopPage(){
+function ItemShopPage() {
 
+  return (
+    <>
+      <h1>상점 페이지: Store</h1>
+    </>
+  );
 }
 
 export default ItemShopPage;

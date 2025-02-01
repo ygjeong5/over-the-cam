73b331f684f0b4f8b5179b55912150345fe82fcf
asj7090 <asj7090@gmail.com>
2025-02-01 22:38:12 +0900

    :bug: fix: 필드명 수정

--- backend/overthecam/src/main/java/com/overthecam/battle/repository/BattleRepository.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/repository/BattleRepository.java
@@ -4,5 +4,5 @@ import com.overthecam.battle.domain.Battle;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BattleRepository extends JpaRepository<Battle, Long> {
-    long countUsersByBattleId(Long battleId);  // 참가자 수 카운트 메서드
+    long countUsersById(Long battleId);  // 참가자 수 카운트 메서드
 }

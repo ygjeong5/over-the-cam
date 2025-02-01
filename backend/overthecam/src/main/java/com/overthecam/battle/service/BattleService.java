8dc6f6cf855c5547a97f7ac7da62ac285da739ac
ghkdtnql <ghkdtnql@gmail.com>
2025-02-01 12:43:47 +0900

    🔧feat: Openvidu에서 session, token 생성하는 service 구현

--- backend/overthecam/src/main/java/com/overthecam/battle/service/BattleService.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/service/BattleService.java
@@ -1,25 +1,15 @@
 package com.overthecam.battle.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
-import com.overthecam.battle.domain.Battle;
 import com.overthecam.battle.dto.BattleCreateRequest;
 import com.overthecam.battle.dto.BattleResponse;
-import com.overthecam.battle.repository.BattleRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.*;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Base64;
-import java.util.HashMap;
-import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class BattleService {
 
-    private final OpenviduSer
+    private final OpenViduService openViduService;
 
     public BattleResponse createBattleRoom(BattleCreateRequest request, String authToken) {
 
--- backend/overthecam/src/main/java/com/overthecam/battle/service/OpenViduService.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/service/OpenViduService.java
@@ -1,6 +1,34 @@
 package com.overthecam.battle.service;
 
+import io.openvidu.java.client.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class OpenViduService {
 
+    // OpenVidu 서버와의 연결을 위한 클라이언트 객체
     private final OpenVidu openVidu;
+
+    //OpenVidu 화상 채팅 세션 생성
+    public Session createSession() throws OpenViduJavaClientException, OpenViduHttpException {
+        SessionProperties properties = new SessionProperties.Builder().build();
+        return openVidu.createSession(properties);
+    }
+
+    //특정 세션에 대한 사용자 토큰 생성
+    //클라이언트가 OpenVidu 세션에 접속할 때 사용
+    public String createConnection(String sessionId) throws OpenViduJavaClientException, OpenViduHttpException {
+        // 주어진 세션 ID로 활성 세션을 조회
+        Session session = openVidu.getActiveSession(sessionId);
+        ConnectionProperties properties = new ConnectionProperties.Builder()
+                .type(ConnectionType.WEBRTC)
+                .role(OpenViduRole.PUBLISHER)
+                .build();
+        Connection connection = session.createConnection(properties);
+        return connection.getToken();
+    }
+
+
 }

f9a44927de2836b24e72fd284f3979be2ed0410c
shsim <sh227sim@gmail.com>
2025-02-03 00:59:28 +0900

    ✨ feat: 배틀 방 목록 조회 페이지 페이지네이션 적용

--- frontend/overthecam/src/page/BattleRoom/BattleMainPage.jsx
+++ frontend/overthecam/src/page/BattleRoom/BattleMainPage.jsx
@@ -1,30 +1,48 @@
 import { useState, useEffect, useCallback } from "react";
 import { Link } from "react-router-dom";
-import { OpenVidu } from "openvidu-browser";
-
+import Pagination from "react-js-pagination";
 import BattleListItem from "../../components/BattleRoom/BattleListItem";
 
 function BattleMainPage() {
+  // 현재 페이지
+  const [page, setPage] = useState(1);
   const [battles, setBattles] = useState([
     {
       id: 1234,
       sessionId: "session-1234",
-      BattleName: "깻잎 논쟁쟁",
+      BattleName: "깻잎 논쟁",
     },
     {
       id: 2345,
       sessionId: "session-2345",
-      BattleName: "피자 vs 치킨킨",
+      BattleName: "피자 vs 치킨",
     },
   ]);
+  const [currentList, setCurrentList] = useState(battles);
+  // 한 페이지당 8개 보여준다
+  const itemsPerPage = 1;
+  // slice할 index범위
+  const indexOfLastItem = page * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+
+  useEffect(() => {
+    setCurrentList(battles.slice(indexOfFirstItem, indexOfLastItem));
+  }, [page, battles]);
+
+  const changePageHandler = (page) => {
+    setPage(page);
+  };
 
   return (
     <>
       <Link to={"/create-battle-room"}>방만들기</Link>
-      <h1>Battle Main Page</h1>
       <div>
-        <p>배틀 방 정보가 담긴 컴포넌트 목록 배열</p>
-        {battles.map((post) => (
+        {/* 타이틀 섹션 */}
+        <h1>Battle Main Page</h1>
+      </div>
+      <div>
+        {/* 배틀 목록 컴포넌트 생성 */}
+        {currentList.map((post) => (
           <BattleListItem
             key={post.id}
             sessionId={post.sessionId}
@@ -32,6 +50,15 @@ function BattleMainPage() {
           />
         ))}
       </div>
+      <Pagination
+        activePage={page}
+        itemsCountPerPage={itemsPerPage}
+        totalItemsCount={battles.length}
+        pageRangeDisplayed={5}
+        prevPageText={"이전"}
+        nextPageText={"다음"}
+        onChange={changePageHandler}
+      />
     </>
   );
 }

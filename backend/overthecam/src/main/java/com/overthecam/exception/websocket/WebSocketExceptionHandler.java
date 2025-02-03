8ec53ac38bf8532c44750158562ea3aaa7e0b94b
asj7090 <asj7090@gmail.com>
2025-02-03 16:42:26 +0900

    :bug: fix: 웹소켓 예외처리 응답 개선

--- backend/overthecam/src/main/java/com/overthecam/exception/websocket/WebSocketExceptionHandler.java
+++ backend/overthecam/src/main/java/com/overthecam/exception/websocket/WebSocketExceptionHandler.java
@@ -1,10 +1,8 @@
 package com.overthecam.exception.websocket;
 
 import com.overthecam.websocket.dto.WebSocketResponseDto;
-import java.security.Principal;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,30 +10,11 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class WebSocketExceptionHandler {
 
-//    @MessageExceptionHandler(WebSocketException.class)
-//    @SendToUser("/queue/errors")
-//    public WebSocketResponseDto<?> handleStompException(WebSocketException ex) {
-//        log.error("WebSocket 통신 오류: {}", ex.getMessage());
-//        return WebSocketResponseDto.error(ex.getErrorCode());
-//    }
-
-    private final SimpMessagingTemplate messagingTemplate;
-
-    public WebSocketExceptionHandler(SimpMessagingTemplate messagingTemplate) {
-        this.messagingTemplate = messagingTemplate;
-    }
-
     @MessageExceptionHandler(WebSocketException.class)
-    public void handleWebSocketException(WebSocketException ex, Principal principal) {
+    @SendToUser(destinations = "/queue/errors", broadcast = false)
+    public WebSocketResponseDto<?> handleWebSocketException(WebSocketException ex) {
         log.error("WebSocket 통신 오류: {}", ex.getMessage());
-
-        if (principal != null) {
-            WebSocketResponseDto<?> errorResponse = WebSocketResponseDto.error(ex.getErrorCode());
-            messagingTemplate.convertAndSendToUser(
-                principal.getName(),
-                "/queue/errors",  // 클라이언트는 /user/queue/errors를 구독해야 함
-                errorResponse
-            );
-        }
+        return WebSocketResponseDto.error(ex.getErrorCode());
     }
+
 }

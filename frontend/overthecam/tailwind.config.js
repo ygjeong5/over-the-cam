eaf578646208c8eefaadbbb40ae87b72bd199630
Sunhwa Sim <sh227sim@gmail.com>
2025-02-05 20:38:21 +0900

    💄 design: 커스텀 컬러 추가가

--- frontend/overthecam/tailwind.config.js
+++ frontend/overthecam/tailwind.config.js
@@ -9,12 +9,15 @@ export default {
       colors: {
         cusRed: {
           DEFAULT: "rgb(255, 92, 92)",
+          light: "rgb(255, 127, 127)",
         },
         cusPink: {
           DEFAULT: "rgb(255, 210, 210)",
+          light: "rgb(255, 214, 214)",
         },
         cusBlue: {
           DEFAULT: "rgb(92, 157, 255)",
+          light: "rgb(180,212,255)",
         },
         cusLightBlue: {
           DEFAULT: "rgb(182, 212, 253)",
@@ -22,25 +25,35 @@ export default {
         },
         cusGray: {
           DEFAULT: "rgb(238,238,238)",
+          light: "rgb(245,245,245)",
         },
         cusYellow: {
           DEFAULT: "rgb(255, 249, 163)",
         },
         cusBlack: {
           DEFAULT: "rgb(33, 33, 33)",
+          light: "rgb(77, 77, 77)",
         },
         success: {
-          bg: "rgb(182, 212, 253)",
-          DEFAULT: "rgb(92, 157, 255)",
+          DEFAULT: "rgb(128, 230, 98)",
         },
         error: {
-          bg: "rgb(255, 210, 210)",
           DEFAULT: "rgb(255, 92, 92)",
+        },
+        btnLightBlue: {
+          DEFAULT: "rgb(180,212,255)",
+          hover: "rgb(118, 177, 255)",
+        },
+        btnPink: {
+          DEFAULT: "rgb(255, 210, 210)",
+          hover: "rgb(252, 160, 160)",
+        },
+        btnYellow: {
+          DEFAULT: "rgb(255, 249, 163)",
+          hover: "rgb(252, 243, 121)",
         },
       },
     }, // 테마 확장
   },
-  plugins: [
-    require('tailwind-scrollbar-hide'),
-  ], // 플러그인 추가
+  plugins: [require("tailwind-scrollbar-hide")], // 플러그인 추가
 };

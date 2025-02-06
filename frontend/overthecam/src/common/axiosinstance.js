53d754637eb2c84a5a5cf7e6bd1ca4d6121769f3
Sunhwa Sim <sh227sim@gmail.com>
2025-02-06 16:41:24 +0900

    🧑‍💻 feat: axios 인스턴스 업데이트트

--- frontend/overthecam/src/common/axiosinstance.js
+++ frontend/overthecam/src/common/axiosinstance.js
@@ -6,17 +6,44 @@ const authAxios = axios.create({
 
 authAxios.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token"); // 저장된 토큰 가져오기, cookie 로 수정 필요
+    const token = localStorage.getItem("token"); // 저장된 토큰 가져오기
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
+      config.headers["Content-Type"] = "application/json";
     }
     return config;
   },
   (error) => Promise.reject(error)
 );
 
+authAxios.interceptors.response.use(
+  (response) => {
+    return response.data.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
 const publicAxios = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
 });
+
+publicAxios.interceptors.request.use(
+  (config) => {
+    config.headers["Content-Type"] = "application/json";
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+publicAxios.interceptors.response.use(
+  (response) => {
+    return response.data.data;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export { authAxios, publicAxios };

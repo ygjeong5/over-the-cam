9131dec6162fca3885c08f00af2593d74149778a
신해인 <godls0215@naver.com>
2025-02-06 11:13:25 +0900

    feat: 진행중, 종료된 투표 컴포넌트 설계, Detail 및 create 페이지 설계

---
+++ frontend/overthecam/src/page/Vote/VoteClosedPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function VoteClosedPage() {
+  return (
+    <div>VoteClosedPage</div>
+  )
+}
---
+++ frontend/overthecam/src/page/Vote/VoteInProgressPage.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function VoteInProgressPage() {
+  return (
+    <div>VoteInProgressPage</div>
+  )
+}

6563f8d3d7a3cc3d6d0045d87a91cdd7381d8b4d
Sunhwa Sim <sh227sim@gmail.com>
2025-02-08 00:52:04 +0900

    ✨ feat: 아이템샵 axios 요청 수정 및 페이지네이션 구현현

--- frontend/overthecam/src/components/ItemShop/MyInventory.jsx
+++ frontend/overthecam/src/components/ItemShop/MyInventory.jsx
@@ -25,16 +25,59 @@ function MyInventory() {
       imageUrl: "",
       type: 1,
     },
+    {
+      name: "기본프레임",
+      price: 200,
+      detail: "프레임 입니다",
+      imageUrl: "",
+      type: 0,
+    },
+    {
+      name: "박수소리",
+      price: 300,
+      detail: "박수를 칩니다",
+      imageUrl: "",
+      type: 1,
+    },
+    {
+      name: "기본프레임",
+      price: 200,
+      detail: "프레임 입니다",
+      imageUrl: "",
+      type: 0,
+    },
+    {
+      name: "박수소리",
+      price: 300,
+      detail: "박수를 칩니다",
+      imageUrl: "",
+      type: 1,
+    },
   ]);
   const [filteredMyItems, setFilteredMyItems] = useState(myItems);
-  const [page, setPage] = useState(1);
-  const [currentList, setCurrentList] = useState([]);
-  // 한 페이지당 8개 보여준다
-  const itemsPerPage = 1;
-  // slice할 index범위
-  const indexOfLastItem = page * itemsPerPage;
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
 
+  // 페이지네이션 구현
+  const [page, setPage] = useState(1);
+  const itemsPerPage = 4;
+  const totalPages = Math.ceil(filteredMyItems.length / itemsPerPage);
+  const paginatedItems = filteredMyItems.slice(
+    page * itemsPerPage - itemsPerPage,
+    page * itemsPerPage
+  );
+
+  const nextPage = () => {
+    if (page < totalPages) {
+      setPage(page + 1);
+    }
+  };
+
+  const prevPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  // 필터 변경
   useEffect(() => {
     const filteredItems =
       filter === 3 ? myItems : myItems.filter((item) => item.type === filter);
@@ -42,21 +85,10 @@ function MyInventory() {
   }, [myItems, filter]);
 
   useEffect(() => {
-    // 페이지에 맞는 항목만 선택하여 currentList에 설정
-    setCurrentList(filteredMyItems.slice(indexOfFirstItem, indexOfLastItem));
-  }, [filteredMyItems, page]); // 필터링된 항목과 페이지가 변경될 때마다 실행
-
-  const changePageHandler = (page) => {
-    setPage(page);
-  };
-
-  useEffect(() => {
     // 내 포인트, 응원 점수, 내가 가진 아이템 목록 받아오기
     getMyInventory()
       .then((res) => {
-        setMyItems(res.data.items);
-        setMyCheerScore(res.data.score); // response 양식 보고 수정할 것
-        setMyPoints(res.data.points); // response 양식 보고 수정할 것
+        setMyItems(res.data);
       })
       .catch((error) => {
         console.log("데이터 불러오기 실패", error);
@@ -66,9 +98,12 @@ function MyInventory() {
       });
   }, []);
 
+  // 모달창 관련 이벤트 함수
   const onShowModal = () => {
     exchangeDialog.current.showModal();
   };
+
+  // 오디오 재생 시작
   const handlePlay = () => {
     const audioElement = document.getElementById("audio-player");
     if (audioElement) {
@@ -77,6 +112,7 @@ function MyInventory() {
     }
   };
 
+  // 오디오 재생 중지
   const handlePause = () => {
     const audioElement = document.getElementById("audio-player");
     if (audioElement) {
@@ -127,85 +163,89 @@ function MyInventory() {
           onSuccess={handleExchange}
         />
         <div className="my-inventory w-3/4 mt-10 mr-10 ml-5 mb-10">
-          <div className="pagenation-btn flex justify-center items-center">
-            <Pagination
-              activePage={page}
-              itemsCountPerPage={itemsPerPage}
-              totalItemsCount={filteredMyItems.length} // 필터링된 항목 개수로 페이지네이션 설정
-              pageRangeDisplayed={0}
-              onChange={changePageHandler}
-              containerClassName={"pagination"}
-              pageLinkClassName={"pagination__link"}
-              activeLinkClassName={"pagination__link__active"}
-            />
-          </div>
           <div className="my-inventory-box flex">
-            <div className="my-inventroy-category w-1/6">
-              <div className="category-button-list">
-                <button
-                  onClick={() => {
-                    setFilter(3);
-                    setPage(1);
-                  }}
-                  className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
-                >
-                  전체보기
-                </button>
-                <button
-                  onClick={() => {
-                    setFilter(0);
-                    setPage(1);
-                  }}
-                >
-                  프레임
-                </button>
-                <button
-                  onClick={() => {
-                    setFilter(1);
-                    setPage(1);
-                  }}
-                >
-                  효과음
-                </button>
-                <button
-                  onClick={() => {
-                    setFilter(2);
-                    setPage(1);
-                  }}
-                >
-                  가면
-                </button>
-              </div>
+            <div>
+              <button
+                onClick={prevPage}
+                disabled={page === 1}
+                className={`px-3 py-2 bg-blue-500 text-white rounded-lg 
+        transition-all duration-300 disabled:bg-gray-300`}
+              >
+                ◀
+              </button>
+              <button
+                onClick={nextPage}
+                disabled={page === totalPages}
+                className={`px-3 py-2 bg-blue-500 text-white rounded-lg 
+        transition-all duration-300 disabled:bg-gray-300`}
+              >
+                ▶
+              </button>
             </div>
-            <div className="my-inventory-container">
-              <div className="my-inventory-content">
-                {isLoading ? (
-                  <>
-                    <p>...Loading</p>
-                  </>
-                ) : currentList.length > 0 ? (
-                  currentList.map((item, i) => (
-                    <div key={i}>
-                      {item.type === 1 ? (
-                        <>
-                          <p>{item.name}</p> <audio id="audio-player" />
-                          <button
-                            onClick={isPlaying ? handlePause : handlePlay}
-                          >
-                            {isPlaying ? "⏸️" : "▶️"}
-                          </button>
-                        </>
-                      ) : (
-                        <>
-                          <p>{item.name}</p> <img />
-                        </>
-                      )}
-                    </div>
-                  ))
-                ) : (
-                  <p>해당 카테고리에 상품이 없습니다.</p>
-                )}
-              </div>
+          </div>
+          <div className="my-inventroy-category w-1/6">
+            <div className="category-button-list">
+              <button
+                onClick={() => {
+                  setFilter(3);
+                  setPage(1);
+                }}
+                className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 transition-colors"
+              >
+                전체보기
+              </button>
+              <button
+                onClick={() => {
+                  setFilter(0);
+                  setPage(1);
+                }}
+              >
+                프레임
+              </button>
+              <button
+                onClick={() => {
+                  setFilter(1);
+                  setPage(1);
+                }}
+              >
+                효과음
+              </button>
+              <button
+                onClick={() => {
+                  setFilter(2);
+                  setPage(1);
+                }}
+              >
+                가면
+              </button>
+            </div>
+          </div>
+          <div className="my-inventory-container overflow-y-auto h-50 w-5/6">
+            <div className="my-inventory-content">
+              {isLoading ? (
+                <>
+                  <p>...Loading</p>
+                </>
+              ) : filteredMyItems.length > 0 ? (
+                paginatedItems.map((item, i) => (
+                  <div key={i}>
+                    {item.type === 1 ? (
+                      <>
+                        <p>{item.name}</p> <audio id="audio-player" />
+                        <button onClick={isPlaying ? handlePause : handlePlay}>
+                          {isPlaying ? "⏸️" : "▶️"}
+                        </button>
+                      </>
+                    ) : (
+                      <>
+                        <p>{item.name}</p> <img />
+                      </>
+                    )}
+                  </div>
+                ))
+              ) : (
+                <p>해당 카테고리에 상품이 없습니다.</p>
+              )}
             </div>
           </div>
         </div>

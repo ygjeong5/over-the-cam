51e1eecd3a0f1be315e5fa2e2930dc61c1e688d1
ghkdtnql <ghkdtnql@gmail.com>
2025-02-07 22:07:07 +0900

    🐛fix: cors를 csrfã보다 먼 설정해ì필록 수정

--- backend/overthecam/src/main/java/com/overthecam/member/controller/MyPageController.java
+++ backend/overthecam/src/main/java/com/overthecam/member/controller/MyPageController.java
@@ -1,16 +1,13 @@
 package com.overthecam.member.controller;
 
-import com.overthecam.common.dto.CommonResponseDto;
-import com.overthecam.member.dto.UserScoreInfo;
-import com.overthecam.member.service.MyPageService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/api/mypage")
 public class MyPageController {
 
-    public CommonResponseDto<UserScoreInfo> revertScoreToPoint(){
-        UserScoreInfo response = MyPageService.revertScoreToPoint();
-        return CommonResponseDto.success()
-    }
+//    public CommonResponseDto<UserScoreInfo> revertScoreToPoint(){
+//        UserScoreInfo response = MyPageService.revertScoreToPoint();
+//        return CommonResponseDto.success()
+//    }
 
 }
--- backend/overthecam/src/main/java/com/overthecam/member/service/MyPageService.java
+++ backend/overthecam/src/main/java/com/overthecam/member/service/MyPageService.java
@@ -1,12 +1,11 @@
 package com.overthecam.member.service;
 
-import com.overthecam.member.dto.UserScoreInfo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MyPageService {
 
-    public static UserScoreInfo revertScoreToPoint() {
-
-    }
+//    public static UserScoreInfo revertScoreToPoint() {
+//
+//    }
 }

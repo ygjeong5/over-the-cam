b437be403aad2a3aaefbccefd4a16ca86e2afcdd
Sunhwa Sim <sh227sim@gmail.com>
2025-02-07 16:36:08 +0900

    🐛 fix: 상점 axios 요청

--- frontend/overthecam/src/service/ItemShop/api.js
+++ frontend/overthecam/src/service/ItemShop/api.js
@@ -1,43 +1,42 @@
-import { AxiosHeaders } from "axios";
 import { authAxios } from "../../common/axiosinstance";
 
-export const getItem = async (setIsLoading) => {
+export const getItem = async () => {
   try {
-    // const response = await authAxios.get("/store/items/all");
-    setIsLoading(false);
+    const response = await authAxios.get("/store/item/all");
     return response;
   } catch (error) {
     console.error("아이템 가져오기 실패", error);
   }
 };
 
-export const getMyInventory = async (setIsLoading) => {
+export const getMyInventory = async () => {
   try {
-    // const response = await authAxios.get("/store/purchases");
-    setIsLoading(false);
+    const response = await authAxios.get("/store/item/my/all");
     return response;
   } catch (error) {
     console.error("내 인벤토리 가져오기 실패", error);
   }
 };
 
-export const postPurchase = async (itemId) => {
+export const postPurchase = async (storeItemId) => {
   try {
-    // const response = await authAxios.post("/store/purchase", {
-    //   itemId,
-    // });
-    return response;    
+    const response = await authAxios.post(
+      `/store/item/${storeItemId}/purchase`,
+      {}
+    );
+    return response;
   } catch (error) {
     console.error("구매 실패", error);
   }
-}
+};
 
 export const postExchangePoints = async (score) => {
   try {
     // const response = await authAxios.post("/points/convert ", {
     //   score,
     // })
+    // return response;
   } catch (error) {
     console.error("환전 실패", error);
   }
-}
+};

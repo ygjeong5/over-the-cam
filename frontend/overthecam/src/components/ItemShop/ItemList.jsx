d390e383bca9a9a301bc332b033d5cd148831c95
Sunhwa Sim <sh227sim@gmail.com>
2025-02-07 17:43:58 +0900

    💄 design: 아이템 리스트 디자인 수정

--- frontend/overthecam/src/components/ItemShop/ItemList.jsx
+++ frontend/overthecam/src/components/ItemShop/ItemList.jsx
@@ -56,8 +56,8 @@ function ItemList() {
   };
 
   return (
-    <>
-      <div>
+    <div className="item-list">
+      <div className="flex felx-col justify-center">
         <button
           onClick={() => {
             setFilter("all");
@@ -68,7 +68,7 @@ function ItemList() {
         </button>
         <button
           onClick={() => {
-          setFilter("FRAME");
+            setFilter("FRAME");
             setPage(1);
           }}
         >
@@ -91,7 +91,7 @@ function ItemList() {
           가면
         </button>
       </div>
-      <div>
+      <div className="flex felx-col justify-center">
         <button
           onClick={() => {
             setOrder(0);
@@ -109,29 +109,47 @@ function ItemList() {
           포인트 높은 순
         </button>
       </div>
-      <h5>상품 전체 보기 페이지 입니다.</h5>
-      {/* 로딩 중이면 로딩 메시지 표시 */}
-      {isLoading ? (
-        <p>로딩 중...</p>
-      ) : currentList.length > 0 ? (
-        currentList.map((item, i) => (
-          <div key={i}>
-            <ItemListItem itemInfo={item} />
+      <div className="h-screen overflow-y-auto">
+        {/* 로딩 중이면 로딩 메시지 표시 */}
+        {isLoading ? (
+          <div className="p-6 m-6 flex justify-center items-center space-x-3">
+            {/* 스피너 */}
+            <div className="loading"></div>
+            {/* 로딩 중 글씨 */}
+            <p className="text-xl font-semibold text-cusBlue dark:text-white drop-shadow-lg">
+              로딩 중...
+            </p>
           </div>
-        ))
-      ) : (
-        <p>해당 카테고리에 상품이 없습니다.</p>
-      )}
-      <Pagination
-        activePage={page}
-        itemsCountPerPage={itemsPerPage}
-        totalItemsCount={filteredItems.length}
-        pageRangeDisplayed={5}
-        prevPageText={"이전"}
-        nextPageText={"다음"}
-        onChange={changePageHandler}
-      />
-    </>
+        ) : currentList.length > 0 ? (
+          <div className="grid grid-cols-2 gap-4">
+            {currentList.map((item, i) => (
+              <div key={i}>
+                <ItemListItem itemInfo={item} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p className="text-xl font-semibold text-cusBlue dark:text-white drop-shadow-lg">
+            해당 카테고리에 상품이 없습니다.
+          </p>
+        )}
+      </div>
+      <div className="flex justify-center pb-10">
+        <Pagination
+          activePage={page}
+          itemsCountPerPage={itemsPerPage}
+          totalItemsCount={filteredItems.length}
+          pageRangeDisplayed={5}
+          prevPageText={"이전"}
+          nextPageText={"다음"}
+          onChange={changePageHandler}
+          innerClass="flex gap-2"
+          itemClass="px-4 py-2 rounded-lg text-cusBlack-light hover:bg-gray-300 transition"
+          activeClass="bg-cusBlack-light !text-white"
+          linkClass="block w-full h-full text-center"
+        />
+      </div>
+    </div>
   );
 }
 

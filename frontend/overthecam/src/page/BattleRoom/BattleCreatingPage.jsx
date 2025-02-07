470b03c46f7797939e9ae7d880fbf8280dcf81e4
Sunhwa Sim <sh227sim@gmail.com>
2025-02-07 14:37:01 +0900

    💄 design: 디자인용 컴포넌트 추가가

--- frontend/overthecam/src/page/BattleRoom/BattleCreatingPage.jsx
+++ frontend/overthecam/src/page/BattleRoom/BattleCreatingPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { createRoom } from "../../service/BattleRoom/api";
 
 import BattleCreateForm from "../../components/BattleRoom/BattleCreatingForm";
+import CursorMotionEffect from "../../components/Layout/CusorMotionDesign";
 
 function BattleCreatingPage() {
   const [battleTitle, setBattleTitle] = useState("");
@@ -31,6 +32,7 @@ function BattleCreatingPage() {
 
   return (
     <div>
+      <CursorMotionEffect/>
       <h1>방 만들기</h1>
       <p>지금 바로 배틀 방을 만들고 논쟁을 즐겨보세요!</p>
       <BattleCreateForm onCreateRoom={createBattleRoomHandler}/>

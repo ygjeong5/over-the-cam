69954d11239d540a257801a727e0e2e9e4259bc1
ghkdtnql <ghkdtnql@gmail.com>
2025-02-08 12:46:29 +0900

    💚test: openvidu url test

--- backend/overthecam/src/main/java/com/overthecam/battle/config/OpenViduConfig.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/config/OpenViduConfig.java
@@ -1,11 +1,13 @@
 package com.overthecam.battle.config;
 
 import io.openvidu.java.client.OpenVidu;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class OpenViduConfig {
 
     @Value("${openvidu.url}")
@@ -16,9 +18,21 @@ public class OpenViduConfig {
 
     @Bean
     public OpenVidu openVidu() {
-        // URL이 https://로 시작하는지 확인하고 필요시 추가
-        System.out.println(OPENVIDU_URL);
-        return new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
+        log.info("OpenVidu URL: {}", OPENVIDU_URL);
+        log.debug("OpenVidu Secret: {}", OPENVIDU_SECRET);
+
+        String finalUrl = OPENVIDU_URL;
+        if (!finalUrl.contains(":4443")) {
+            finalUrl = finalUrl + ":4443";
+            log.info("Added port to URL: {}", finalUrl);
+        }
+
+//        if (!finalUrl.startsWith("https://")) {
+//            finalUrl = "https://" + finalUrl;
+//            log.info("Added https to URL: {}", finalUrl);
+//        }
+
+        return new OpenVidu(finalUrl, OPENVIDU_SECRET);
     }
 
 }

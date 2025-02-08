8da4811e1abe156dbd4020f20c92fe95fd2024b1
shsim <sh227sim@gmail.com>
2025-02-08 23:29:15 +0900

    💄 design: 로그인, 로그아웃 버튼 수정

--- frontend/overthecam/src/components/Layout/NavBar.jsx
+++ frontend/overthecam/src/components/Layout/NavBar.jsx
@@ -18,7 +18,7 @@ export default function NavBar() {
     const token = localStorage.getItem("token");
     setIsLoggedIn(!!token);
   }, [localStorage.getItem("token")]); // 토큰 변경을 감지
-  
+
   return (
     <>
       <header className="h-[80px] mb-4">
@@ -74,20 +74,20 @@ export default function NavBar() {
                 </div>
               </>
             ) : (
-              <>
+              <div className="flex gap-3">
                 <Link
                   to="/login"
-                  className="btn px-4 py-1.5 text-sm bg-btnLightBlue text-btnLightBlue-hover rounded-full hover:bg-btnLightBlue-hover hover:text-btnLightBlue text-center"
+                  className="btn px-4 py-1.5 text-md bg-btnLightBlue text-btnLightBlue-hover rounded-full hover:bg-btnLightBlue-hover hover:text-btnLightBlue text-center"
                 >
                   로그인
                 </Link>
                 <Link
                   to="/signup"
-                  className="btn px-4 py-1.5 text-sm bg-btnLightBlue text-btnLightBlue-hover rounded-full hover:bg-btnLightBlue-hover hover:text-btnLightBlue text-center"
+                  className="btn px-4 py-1.5 text-md bg-btnLightBlue text-btnLightBlue-hover rounded-full hover:bg-btnLightBlue-hover hover:text-btnLightBlue text-center"
                 >
                   회원가입
                 </Link>
-              </>
+              </div>
             )}
           </div>
         </div>

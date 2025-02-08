44b07977819881b65a6d45fd4945ddf79e60b299
ghkdtnql <ghkdtnql@gmail.com>
2025-02-08 13:06:01 +0900

    💚test: openvidu url test

--- backend/overthecam/src/main/java/com/overthecam/battle/config/OpenViduConfig.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/config/OpenViduConfig.java
@@ -21,18 +21,10 @@ public class OpenViduConfig {
         log.info("OpenVidu URL: {}", OPENVIDU_URL);
         log.debug("OpenVidu Secret: {}", OPENVIDU_SECRET);
 
-        String finalUrl = OPENVIDU_URL;
-        if (!finalUrl.contains(":4443")) {
-            finalUrl = finalUrl + ":4443";
-            log.info("Added port to URL: {}", finalUrl);
-        }
+        log.info("Added port to URL: {}", OPENVIDU_URL);
 
-//        if (!finalUrl.startsWith("https://")) {
-//            finalUrl = "https://" + finalUrl;
-//            log.info("Added https to URL: {}", finalUrl);
-//        }
 
-        return new OpenVidu(finalUrl, OPENVIDU_SECRET);
+        return new OpenVidu(OPENVIDU_URL, OPENVIDU_SECRET);
     }
 
 }

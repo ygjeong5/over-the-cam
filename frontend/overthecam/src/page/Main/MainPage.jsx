0efad6926c9369aa381d73604a710b7c25f01086
shsim <sh227sim@gmail.com>
2025-02-09 02:22:13 +0900

    ✨ feat: 배틀, 투표 목록 조회 more 버튼 추가

--- frontend/overthecam/src/page/Main/MainPage.jsx
+++ frontend/overthecam/src/page/Main/MainPage.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import SearchBar from "../../components/Main/SearchBar";
-
+import { Link } from "react-router-dom";
 
 const Card = ({ children }) => (
   <div className="bg-white rounded-lg shadow-md p-4 h-32">{children}</div>
 );
 
 const SectionTitle = ({ title }) => (
-  <h2 className="text-3xl font-bold mb-4 pl-8 text-start">{title}</h2>
+  <h2 className="text-3xl font-bold mb-4 pl-8 text-start justify-start">
+    {title}
+  </h2>
 );
 
 const MainPage = () => {
@@ -29,24 +31,44 @@ const MainPage = () => {
         {/* Content */}
         <div className="container mx-auto p-14">
           {/* Battle Section */}
-          <section className="mb-12">
-            <SectionTitle title="Battle" />
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-              {[...Array(6)].map((_, index) => (
-                <Card key={`battle-${index}`} />
-              ))}
+          <section className="flex flex-col mb-12">
+            <div className="flex justify-between items-center">
+              <SectionTitle title="Battle" />
+              <Link
+                to="/battle-list"
+                className="text-cusBlue text-xl font-medium justify-end mr-5"
+              >
+                + More
+              </Link>
+            </div>
+            <div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {[...Array(6)].map((_, index) => (
+                  <Card key={`battle-${index}`} />
+                ))}
+              </div>
             </div>
           </section>
 
           {/* Vote Section */}
-          <section className="mb-12">
-            <SectionTitle title="Vote" />
-            <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-              {[...Array(2)].map((_, index) => (
-                <Card key={`vote-${index}`} />
-              ))}
+          <section className="flex flex-col mb-12">
+            <div className="flex justify-between items-center">
+              <SectionTitle title="Vote" />
+              <Link
+                className="text-cusBlue text-xl font-medium justify-end mr-5"
+              >
+                + More
+              </Link>
+            </div>
+            <div>
+              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {[...Array(6)].map((_, index) => (
+                  <Card key={`battle-${index}`} />
+                ))}
+              </div>
             </div>
           </section>
+
         </div>
       </div>
     </div>

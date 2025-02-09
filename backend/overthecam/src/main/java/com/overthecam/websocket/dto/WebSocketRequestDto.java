fc9848df2393704448b16036c96fdd0f9c16bac1
ssuzyn <asj7090@gmail.com>
2025-02-09 15:41:55 +0900

    :fire: remove: 웹소켓 요청에 배틀id 삭제

--- backend/overthecam/src/main/java/com/overthecam/websocket/dto/WebSocketRequestDto.java
+++ backend/overthecam/src/main/java/com/overthecam/websocket/dto/WebSocketRequestDto.java
@@ -9,24 +9,21 @@ import java.time.LocalDateTime;
 @Getter
 @Builder
 public class WebSocketRequestDto<T> {
-    private Long battleId;  // 배틀 관련 메시지에만 사용
     private MessageType type;
     private T data;
 
     // 채팅 메시지 요청
-    public static WebSocketRequestDto<ChatMessageRequest> chat(Long battleId, String content) {
+    public static WebSocketRequestDto<ChatMessageRequest> chat(String content) {
         return WebSocketRequestDto.<ChatMessageRequest>builder()
                 .type(MessageType.CHAT)
-                .battleId(battleId)
                 .data(new ChatMessageRequest(content, LocalDateTime.now()))
                 .build();
     }
 
     // 배틀 시작 요청
-    public static WebSocketRequestDto<Void> battleStart(Long battleId) {
+    public static WebSocketRequestDto<Void> battleStart() {
         return WebSocketRequestDto.<Void>builder()
                 .type(MessageType.BATTLE_START)
-                .battleId(battleId)
                 .build();
     }
 

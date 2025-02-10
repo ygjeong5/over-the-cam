525579691e2d60cd8689b5389194b94e2933725c
황수비 <ghkdtnql@gmail.com>
2025-02-10 21:57:35 +0900

    🐛fix: openvidu server url 앞 /api 추가

--- backend/overthecam/src/main/java/com/overthecam/battle/controller/OpenviduController.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/controller/OpenviduController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Map;
 
 @CrossOrigin(origins = "*")
-@RestController("/")
+@RestController("/api")
 public class OpenviduController {
 
     @Value("${livekit.api.key}")

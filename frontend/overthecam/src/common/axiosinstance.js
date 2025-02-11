5ae890eadf1f79ffb2bb57fc6c40c2aec3872b4e
Sunhwa Sim <sh227sim@gmail.com>
2025-02-11 13:35:54 +0900

    🐛 fix: axios 인스턴스 수정

--- frontend/overthecam/src/common/axiosinstance.js
+++ frontend/overthecam/src/common/axiosinstance.js
@@ -1,21 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
 const authAxios = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
-  }
+    "Content-Type": "application/json",
+  },
 });
-console.log('authAxios baseURL:', authAxios.defaults.baseURL);  // 여기 추가
+console.log("authAxios baseURL:", authAxios.defaults.baseURL); // 여기 추가
 
 authAxios.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("token")
+    const token = localStorage.getItem("token");
     if (token) {
-      config.headers.Authorization = `Bearer ${token}`
+      config.headers.Authorization = `Bearer ${token}`;
     }
-    return config
+    return config;
   },
   (error) => {
     return Promise.reject(error);
@@ -23,18 +23,17 @@ authAxios.interceptors.request.use(
 );
 
 authAxios.interceptors.response.use(
-  (response) => response,
+  (response) => {
+    console.log(response.data)
+    return response.data;
+  },
   async (error) => {
     if (error.response && error.response.status === 401) {
       // 토큰이 만료되었을 때 처리
-      localStorage.removeItem("token")
+      localStorage.removeItem("token");
       // 로그인 페이지로 리다이렉트
-      window.location.href = "/login"
+      window.location.href = "/login";
     }
-    return Promise.reject(error)
-  },
-  (error) => {
-    // console.log("Response error:", error.response.data);
     return Promise.reject(error.response.data);
   }
 );
@@ -43,8 +42,8 @@ const publicAxios = axios.create({
   baseURL: import.meta.env.VITE_BASE_URL,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 publicAxios.interceptors.request.use(
@@ -57,18 +56,17 @@ publicAxios.interceptors.request.use(
     });
     return config;
   },
-  (error) => Promise.reject(error),
-)
+  (error) => Promise.reject(error)
+);
 
 publicAxios.interceptors.response.use(
   (response) => {
-    return response.data
+    return response.data;
   },
   (error) => {
     return Promise.reject(error.response.data);
   }
 );
-
 
 // // 환경변수 값도 직접 확인
 // console.log('VITE_BASE_URL:', import.meta.env.VITE_BASE_URL);  // 여기 추가

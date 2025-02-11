d961c89b018b17988b050bce8b13d93562fab967
황수비 <ghkdtnql@gmail.com>
2025-02-12 00:43:34 +0900

    🐛fix: cors 설정 url 추가

--- backend/overthecam/src/main/java/com/overthecam/security/config/SecurityConfig.java
+++ backend/overthecam/src/main/java/com/overthecam/security/config/SecurityConfig.java
@@ -72,7 +72,8 @@ public class SecurityConfig {
                 "https://overthecam.site",
                 "https://overthecam.site:15555",
                 "http://127.0.0.1:5500",
-                "http://localhost:5173"
+                "https://localhost:5173",
+                "https://localhost:5174"
         ));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));

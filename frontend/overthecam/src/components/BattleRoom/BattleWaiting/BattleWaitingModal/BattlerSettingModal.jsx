bd8aa7eb510d929bc74027372937be81a854dd2f
shsim <sh227sim@gmail.com>
2025-02-12 23:35:42 +0900

    ✨ feat: 배틀러 선정 모달창 생성

--- frontend/overthecam/src/components/BattleRoom/BattleWaiting/BattleWaitingModal/BattlerSettingModal.jsx
+++ frontend/overthecam/src/components/BattleRoom/BattleWaiting/BattleWaitingModal/BattlerSettingModal.jsx
@@ -1,16 +1,64 @@
-import { forwardRef, useRef, useState } from "react";
+import { forwardRef, useImperativeHandle, useRef, useState } from "react";
+import FailAlertModal from "../../../@common/FailAlertModal";
 
 const BattlerSettingModal = forwardRef(function BattlerSettingModal(_, ref) {
   const modalRef = useRef();
-  const FailToast = useRef();
+  const failToast = useRef();
   // api response로 선택지, user 정보 넘겨주기
 
   const [battlers, setBattlers] = useState([]);
-  const [participants, setParticipants] = useState([]);
+  const [participants, setParticipants] = useState([
+    {
+      participantName: "우끼끼 정해기",
+    },
+    {
+      participantName: "빽다방구라치노",
+    },
+    {
+      participantName: "우르르롹끼",
+    },
+    {
+      participantName: "붐샤카라카",
+    },
+  ]);
+
+  const handleChange = (e) => {};
+
+  useImperativeHandle(ref, () => ({
+    showModal: (option1, option2) => {
+      try {
+        modalRef.current?.showModal();
+        // 배틀러 두명이 잘 선정 되었는지?
+      } catch (error) {
+        console.error("모달 표시 중 에러:", error);
+      }
+    },
+  }));
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      // 유효성 검사
+      // api 검사해서 post 요청
+      // 성공 하면 모달 닫기
+      modalRef.current?.close();
+    } catch (error) {
+      console.error("투표 생성 중 에러:", error);
+      if (modalRef.current) {
+        modalRef.current.close();
+      }
+      if (failToast.current && failToast.current.showAlert) {
+        failToast.current?.showAlert("배틀러를 선정하지 못했습니다.");
+      } else {
+        console.error("failAlertRef.current나 showAlert 메서드가 없습니다.");
+      }
+    }
+  }
 
   return (
     <>
-      <dialog>
+      <dialog ref={modalRef}>
         <div>
           <h1>배틀러 선정</h1>
         </div>
@@ -23,6 +71,7 @@ const BattlerSettingModal = forwardRef(function BattlerSettingModal(_, ref) {
           </div>
         </div>
       </dialog>
+      <FailAlertModal ref={failToast}/>
     </>
   );
 });

a261e3d9a04e916f759d809dd1ce5838ca437a91
asj7090 <asj7090@gmail.com>
2025-02-12 13:55:55 +0900

    :recycle: refactor: 사용자 응원점수 관련 서비스 패키지 구조 변경

--- backend/overthecam/src/main/java/com/overthecam/member/service/UserScoreService.java
+++ backend/overthecam/src/main/java/com/overthecam/member/service/UserScoreService.java
@@ -1,9 +1,12 @@
 package com.overthecam.member.service;
 
 import com.overthecam.auth.repository.UserRepository;
+import com.overthecam.websocket.exception.WebSocketErrorCode;
+import com.overthecam.websocket.exception.WebSocketException;
 import com.overthecam.member.dto.UserScoreInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
@@ -16,4 +19,25 @@ public class UserScoreService {
         return userRepository.findScoreAndPointByUserId(userId);
     }
 
+    @Transactional
+    public UserScoreInfo updateSupportScore(Long userId, Integer score) {
+        UserScoreInfo userScoreInfo = userRepository.findScoreAndPointByUserId(userId).get();
+        int newScore = userScoreInfo.getSupportScore() - score;
+        if (newScore < 0) {
+            throw new WebSocketException(WebSocketErrorCode.INSUFFICIENT_SCORE, "응원 점수가 부족합니다.");
+        }
+        userRepository.updateSupportScore(userId, newScore);
+        return UserScoreInfo.updateSupportScore(newScore);
+    }
+
+    @Transactional
+    public UserScoreInfo updatePoints(Long userId, Integer points) {
+        UserScoreInfo userScoreInfo = userRepository.findScoreAndPointByUserId(userId).get();
+        int newPoints = userScoreInfo.getPoint() - points;
+        if (newPoints < 0) {
+            throw new WebSocketException(WebSocketErrorCode.INSUFFICIENT_POINTS, "포인트가 부족합니다.");
+        }
+        userRepository.updatePoint(userId, newPoints);
+        return UserScoreInfo.updatePoints(newPoints);
+    }
 }

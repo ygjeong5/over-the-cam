8141b954a57087340fd4510b6e068a411b69d7cd
ssuzyn <asj7090@gmail.com>
2025-02-15 01:16:37 +0900

    :sparkles: add: cors 경로 추가

--- backend/overthecam/src/main/java/com/overthecam/websocket/config/WebSocketConfig.java
+++ backend/overthecam/src/main/java/com/overthecam/websocket/config/WebSocketConfig.java
@@ -37,6 +37,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             .setAllowedOrigins(
                 "http://127.0.0.1:5173",
                 "http://localhost:5173",
+                "https://127.0.0.1:5173",
+                "https://localhost:5173",
                 "http://127.0.0.1:5174",
                 "http://localhost:5174",
                 "http://127.0.0.1:5175",

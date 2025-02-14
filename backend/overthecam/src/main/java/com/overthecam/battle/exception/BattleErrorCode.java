d7f9f22c55c128b62e40b3303465259b5aeddc46
ssuzyn <asj7090@gmail.com>
2025-02-14 21:36:48 +0900

    :sparkles: add: 배틀러 선정 관련 에러코드 추가

--- backend/overthecam/src/main/java/com/overthecam/battle/exception/BattleErrorCode.java
+++ backend/overthecam/src/main/java/com/overthecam/battle/exception/BattleErrorCode.java
@@ -29,7 +29,13 @@ public enum BattleErrorCode implements ErrorCode {
     INVALID_ROLE(400, "올바른 배틀 역할이 아닙니다."),
     BATTLER_VOTE_NOT_FOUND(404, "배틀러가 선택한 투표 옵션이 존재하지 않습니다"),
     NOT_PREPARED(400, "배틀 준비가 필요합니다"),
-    INVALID_BATTLE_START(400, "모든 참가자가 준비되지 않았습니다.");
+    INVALID_BATTLE_START(400, "모든 참가자가 준비되지 않았습니다."),
+
+    // 배틀러 선정 관련 에러
+    FAIL_BATTLER_SELECT(400, "배틀러 선정에 실패했습니다. 해당 사용자가 배틀방에 참여하고 있는지 확인해주세요."),
+    INVALID_BATTLER_SELECT(400, "동일한 사용자를 배틀러로 선택할 수 없습니다. 서로 다른 참가자를 선택해주세요."),
+    MISSING_BATTLER_INFO(400, "배틀러 정보가 누락되었습니다. 두 명의 배틀러 정보를 모두 입력해주세요.");
+
 
 
     private final int status;

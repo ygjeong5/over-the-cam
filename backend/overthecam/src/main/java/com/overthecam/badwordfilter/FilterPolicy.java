443b073ebe08bc7bdf192c661603420d72087de3
ssuzyn <asj7090@gmail.com>
2025-02-16 23:48:38 +0900

    :art: chore: 정규 표현식 자음 필터링 제거

--- backend/overthecam/src/main/java/com/overthecam/badwordfilter/FilterPolicy.java
+++ backend/overthecam/src/main/java/com/overthecam/badwordfilter/FilterPolicy.java
@@ -1,6 +1,5 @@
 package com.overthecam.badwordfilter;
 
-import lombok.Getter;
 
 /**
  * 필터링할 패턴 정책을 정의하는 열거형
@@ -9,7 +8,7 @@ public enum FilterPolicy {
     WHITESPACE("[\\s]", FilterType.WHITESPACE),           // 공백 문자
     SPECIAL_CHARS("[^\\p{L}\\p{N}\\s]", FilterType.SPECIAL), // 특수문자
     NUMBERS("[\\p{N}]", FilterType.NUMBER),              // 숫자
-    CONSONANTS("[ㄱ-ㅎ]", FilterType.CONSONANT),         // 자음
+    //CONSONANTS("[ㄱ-ㅎ]", FilterType.CONSONANT),         // 자음
     VOWELS("[ㅏ-ㅣ]", FilterType.VOWEL),                // 모음
     REPEATING("(.)\\1+", FilterType.REPEAT);            // 반복되는 문자
 

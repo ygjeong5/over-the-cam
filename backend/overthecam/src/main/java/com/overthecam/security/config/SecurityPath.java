0180932d32ca3b061745084f7002a08a72687e44
ssuzyn <asj7090@gmail.com>
2025-02-18 00:02:59 +0900

    :sparkles: add: 웹소켓 경로 설정

--- backend/overthecam/src/main/java/com/overthecam/security/config/SecurityPath.java
+++ backend/overthecam/src/main/java/com/overthecam/security/config/SecurityPath.java
@@ -10,6 +10,9 @@ public enum SecurityPath {
 
     // WebSocket 관련 경로
     WS_CONNECT("/api/ws-connect/**"),
+    WS_CONNECT_ROOT("/api/ws-connect"),
+    WS_CONNECT_INFO("/api/ws-connect/info"),
+
 
     // Messaging 관련 경로
     PUBLISH("/api/publish/**"),

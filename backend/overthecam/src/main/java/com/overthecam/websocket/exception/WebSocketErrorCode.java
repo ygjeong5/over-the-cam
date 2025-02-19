d70b8970954ddc0b3c071e6993e99d7b414ec2f5
ssuzyn <asj7090@gmail.com>
2025-02-20 04:54:29 +0900

    Revert ":sparkles: add: 웹소켓 세션 만료 에러코드 추가"
    This reverts commit e8c065600f0e285ac2fa62bae652959c07ecc318.

--- backend/overthecam/src/main/java/com/overthecam/websocket/exception/WebSocketErrorCode.java
+++ backend/overthecam/src/main/java/com/overthecam/websocket/exception/WebSocketErrorCode.java
@@ -35,8 +35,7 @@ public enum WebSocketErrorCode implements ErrorCode {
     INSUFFICIENT_SCORE(400, "응원점수가 부족합니다"),
     INSUFFICIENT_POINT(400, "포인트가 부족합니다"),
 
-    INTERNAL_SERVER_ERROR(500, "서버 내부 오류가 발생했습니다."),
-    INVALID_SESSION(500, "배틀방 세션이 만료되었습니다. 다시 연결해주세요.");
+    INTERNAL_SERVER_ERROR(500, "서버 내부 오류가 발생했습니다.");
 
     private final int status;
     private final String message;
